dd1d655402db9d01855a3a43cf07b071dc671d28
andrijanicam1 <andrijana.misic32@gmail.com>
2022-07-11 21:49:18 +0200

    get all data for single city page

--- src/views/WeatherDetails.vue
+++ src/views/WeatherDetails.vue
@@ -4,35 +4,44 @@
     <div>  
       
 			
-			<div v-if="allDatas.length">
+		<div v-if="allDatas.length">
+
+			<div>
 				<h1> {{name}} </h1>
-				<div> {{allDatas[1].resolvedAddress}} </div>
-				<div> {{allDatas[0][0].latitude}} {{allDatas[0][0].longitude}} </div>
-
-
-				<div> {{allDatas[1].currentConditions.temp}} </div>
-
-				<div> Feels like {{allDatas[1].currentConditions.feelslike}} </div>
-				<div> Humidity {{allDatas[1].currentConditions.humidity}} </div>
-				<div> Pressure {{allDatas[1].currentConditions.pressure}} </div>
-				<div> Wind </div>
-				<div> UV index {{allDatas[1].currentConditions.uvindex}} </div>
-
-					<div> 
-						<div v-for="day in days" :key="day"> 
-							<div> {{formatDate(day.datetime)}} </div>  
-
-							<div class="hourly-weather-wrapper">
-								<div class="hourly-weather" v-for="hour in day.hours" :key="hour">
-									{{hour.datetime}}h 
-									{{hour.temp}}
-								</div> 
-							</div>
-						</div>
-
-					</div>
-
+				<div> {{weatherApiData.resolvedAddress}} </div>
+				<div> {{cityApiData[0].latitude}} {{cityApiData[0].longitude}} </div>
 			</div>
+
+			<div> {{weatherApiData.currentConditions.temp}} </div>
+
+			<div>
+				<div> Feels like {{ weatherApiData.currentConditions.feelslike }} </div>
+				<div> Humidity {{ weatherApiData.currentConditions.humidity }} </div>
+				<div> Pressure {{ weatherApiData.currentConditions.pressure }} </div>
+				<div> Wind {{ weatherApiData.currentConditions.windspeed }} m/s {{ direction }}</div>
+				<div> UV index {{ weatherApiData.currentConditions.uvindex }} </div>
+			</div>
+
+			<div> 
+				<div v-for="day in weatherApiData.days.slice(1,4)" :key="day"> 
+					<div> {{formatDate(day.datetime)}} </div>  
+
+					<div class="hourly-weather-wrapper">
+						<div class="hourly-weather" v-for="hour in day.hours" :key="hour">
+							{{hour.temp}}
+
+							<div class="icon">
+								<img :src = "require(`@/assets/images/icons/${hour.icon}.svg`)" :alt="`${hour.icon}`"/>
+							</div> 
+
+							{{hour.datetime.slice(0,2)}}h  
+						
+						</div> 
+					</div>
+				</div>
+			</div>
+
+		</div>
     </div>
  </div>
 </template>
@@ -40,15 +49,18 @@
 <script>
 
 import API from '@/lib/API';
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref } from 'vue'
 
 export default {
   name: 'WeatherDetails',
   props: ['name'],
   setup(props) {
 
-    let allDatas= ref([]),
-				days = ref([]);
+    let allDatas = ref([]),
+				cityApiData =  ref([]),
+				weatherApiData = ref([]);
+
+		const direction = ref([]);
     
     onMounted( () => {
 			let citiesData = API.getCityName(props.name).then((result) => {
@@ -60,9 +72,16 @@ export default {
 			
 			Promise.all([citiesData, watherData]).then((result) => { 
 					allDatas.value = result;
+					cityApiData.value = result[0];
+					weatherApiData.value = result[1];
 
-					// data for the next 3 day
-					days.value = result[1].days.slice(1,4);
+					let winddir = weatherApiData.value.days[1].winddir;
+
+					console.log(weatherApiData)
+
+					//calculate compass direction
+					direction.value = ['N','NE', 'E','SE', 'S','SW', 'W','NW'][Math.floor(((winddir+22.5)%360)/45)]
+
 			});
     });
 
@@ -74,7 +93,7 @@ export default {
 		}
 
 
-    return { allDatas, formatDate, days }
+    return { allDatas, weatherApiData, cityApiData, direction, formatDate }
   }
 }
 </script>
